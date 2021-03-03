a208a38db629062c98f662a08aee3b0c5552c328
LynnL4 <lht856@foxmail.com>
2021-03-03 09:59:06 +0800

    update project

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
 defmodule NervesSystemBbb.MixProject do
   use Mix.Project
 
-  @github_organization "nerves-project"
-  @app :nerves_system_bbb
+  @github_organization "LynnL4"
+  @app :custom_bbb
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
